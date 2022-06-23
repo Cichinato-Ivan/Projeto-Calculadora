55a8537d23ac6303902d28e40e638a843f091302
ivan cichinato de oliveira <ivan.oliveira01@yahoo.com>
2022-06-22 22:30:35 -0300

    finalizando datas

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -12,9 +12,48 @@ class CalcController {
 
     initialize(){
 
-        this._dateEl.innerHTML = "21/06/2022"
-        this._timeEl.innerHTML = "23:30"
+        this.setDisplayDateTime()
 
+        setInterval(()=>{
+
+            this.setDisplayDateTime()
+
+        }, 1000)
+
+    }
+
+    initButtonsEvents(){
+
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g")
+
+    }
+
+    setDisplayDateTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        })
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+
+    }
+
+
+    get displayTime(){
+        return this._timeEl.innerHTML
+    }
+
+    set displayTime(value){
+        return this._timeEl.innerHTML = value
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML
+    }
+
+    set displayDate(value){
+        return this._dateEl.innerHTML = value
     }
 
     get displayCalc(){
@@ -26,7 +65,7 @@ class CalcController {
     }
 
     get currentDate(){
-        return this._currentDate
+        return new Date()
     }
 
     set currentDate(valor){
